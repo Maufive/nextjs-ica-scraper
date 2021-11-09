6f622df5479182023d1cc300bba7f4df906e247e
Niklas Albinsson <albinssonniklas@gmail.com>
2021-11-09 20:30:18 +0100

    Create input to allow any url

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 const Blog: React.FC = () => {
-  const postRecipie = async () => {
-    const ICA_URL = 'https://www.ica.se/recept/varldens-godaste-appelsmulpaj-714125/';
-    const body = { url: ICA_URL };
+  const [value, setValue] = React.useState('');
+
+  const postRecipie = React.useCallback(async () => {
+    // const ICA_URL = 'https://www.ica.se/recept/varldens-godaste-appelsmulpaj-714125/';
+    const body = { url: value };
+
+    console.log(body);
 
     const response = await fetch('http://localhost:3000/api/recipies', {
       method: 'POST',
@@ -15,14 +19,17 @@ const Blog: React.FC = () => {
     const json = await response.json();
 
     console.log(json);
-  };
+  }, [value]);
 
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
-          <button type="button" onClick={postRecipie}>CLICK ME</button>
+          <form onSubmit={postRecipie}>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+            <button type="submit">CLICK ME</button>
+          </form>
         </main>
       </div>
       <style jsx>
