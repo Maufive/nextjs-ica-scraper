bbeb478d339dfda160336d0f923f919909c8b6f8
Niklas Albinsson <albinssonniklas@gmail.com>
2021-12-05 15:26:02 +0100

    Fetch shoppingLists clientside

---
+++ features/shopping-list/shopping-list-card-skeletons.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import {
+  Flex,
+  Text,
+  useColorModeValue,
+  Skeleton,
+} from '@chakra-ui/react';
+
+const ShoppingListCardSkeleton = () => (
+  <Flex
+    width="100%"
+    align="center"
+    px={4}
+    py={4}
+    rounded="sm"
+    bg={useColorModeValue('gray.100', 'gray.900')}
+  >
+    <Flex
+      flex="2"
+      flexDirection="column"
+      align="flex-start"
+      justify="space-around"
+      marginLeft={4}
+    >
+      <Skeleton w="100%" mb={2}>
+        <Text fontWeight={700}>
+          Hello world!
+        </Text>
+      </Skeleton>
+      <Skeleton>
+        <Text
+          fontWeight={500}
+          fontSize="sm"
+          margin="0"
+        >
+          100 varor
+        </Text>
+      </Skeleton>
+    </Flex>
+  </Flex>
+);
+
+const SkeletonCards: React.FC = () => {
+  const arr = Array(5).fill(null);
+  return (
+    <>
+      {arr.map((_, index) => <ShoppingListCardSkeleton key={index} />)}
+    </>
+  );
+};
+
+export default SkeletonCards;
--- pages/inkopslistor/index.tsx
+++ pages/inkopslistor/index.tsx
@@ -1,40 +1,33 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { NextPage, GetServerSideProps } from 'next';
-import { useSession, getSession } from 'next-auth/client';
+import React, { useEffect } from 'react';
+import { NextPage } from 'next';
+import { useSession } from 'next-auth/client';
 import { VStack, Heading, Text } from '@chakra-ui/react';
-// import prisma from '../../lib/prisma';
 import Layout from '../../components/Layout';
-// import ShoppingListContainer from '../../features/shopping-list/shopping-list-container';
 import ShoppingListCard from '../../features/shopping-list/shopping-list-card';
-import { ShoppingList } from '../../types';
-import prisma from '../../lib/prisma';
-// import { useAppDispatch, useAppSelector } from '../../state/redux-hooks';
-// import {
-//   LoadingStates,
-//   fetchAllShoppingLists,
-//   selectShoppingLists,
-//   selectShoppingListsLoading,
-//   clearShoppingList,
-// } from '../../features/shopping-list/shopping-list-duck';
+import { useAppDispatch, useAppSelector } from '../../state/redux-hooks';
+import {
+  LoadingStates,
+  fetchAllShoppingLists,
+  selectShoppingLists,
+  selectShoppingListsLoading,
+  clearShoppingList,
+} from '../../features/shopping-list/shopping-list-duck';
+import SkeletonCards from '../../features/shopping-list/shopping-list-card-skeletons';
 
-interface PageProps {
-  shoppingLists: ShoppingList[];
-}
-
-const PageShoppingList: NextPage<PageProps> = ({ shoppingLists }) => {
+const PageShoppingList: NextPage = () => {
   const [session, loading] = useSession();
-  // const dispatch = useAppDispatch();
-  // const shoppingListsLoading = useAppSelector(selectShoppingListsLoading);
-  // const shoppingLists = useAppSelector(selectShoppingLists);
+  const dispatch = useAppDispatch();
+  const shoppingListsLoading = useAppSelector(selectShoppingListsLoading);
+  const shoppingLists = useAppSelector(selectShoppingLists);
 
-  // useEffect(() => {
-  //   dispatch(fetchAllShoppingLists());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAllShoppingLists());
+  }, [dispatch]);
 
-  // useEffect(() => {
-  //   dispatch(clearShoppingList());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(clearShoppingList());
+  }, [dispatch]);
 
   if (loading) {
     return (
@@ -60,58 +53,21 @@ const PageShoppingList: NextPage<PageProps> = ({ shoppingLists }) => {
   return (
     <Layout>
       <VStack w="100%">
-        {shoppingLists.map((list) => (
+        {shoppingListsLoading === LoadingStates.PENDING && (
+          <SkeletonCards />
+        )}
+        {shoppingListsLoading === LoadingStates.SUCCESS && shoppingLists.map((list) => (
           <ShoppingListCard
             key={list.id}
             title={list.title}
-                      // eslint-disable-next-line no-underscore-dangle
+            // eslint-disable-next-line no-underscore-dangle
             itemCount={list._count.items}
             id={list.id}
           />
         ))}
-        {/* {shoppingListsLoading === LoadingStates.SUCCESS && shoppingLists.map((list) => (
-
-        ))} */}
       </VStack>
-      {/* <ShoppingListContainer /> */}
     </Layout>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const session = await getSession({ req });
-
-  if (!session) {
-    res.statusCode = 403;
-    return { props: { initialShoppingLists: [] } };
-  }
-
-  const result = await prisma.shoppingList.findMany({
-    where: {
-      author: { email: session.user.email },
-    },
-    include: {
-      _count: {
-        select: {
-          items: true,
-        },
-      },
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-
-  const initialShoppingLists = result.map((s) => ({
-    ...s,
-    createdAt: Date.parse(s.createdAt.toString()),
-  }));
-
-  return {
-    props: {
-      shoppingLists: initialShoppingLists,
-    },
-  };
 };
 
 export default PageShoppingList;
